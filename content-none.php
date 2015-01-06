899c8759597be153c993be19da3e8f402704c10e
Ismael Toe <ismael.toe@gmail.com>
2015-01-06 18:15:45 +0000

    Improvements

---
+++ content-none.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template part for displaying a message that posts cannot be found.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Clean Blog
+ */
+?>
+
+<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+
+	<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'cleanblog' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+
+<?php elseif ( is_search() ) : ?>
+
+	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'cleanblog' ); ?></p>
+	<?php get_search_form(); ?>
+
+<?php else : ?>
+
+	<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'cleanblog' ); ?></p>
+	<?php get_search_form(); ?>
+
+<?php endif; ?>

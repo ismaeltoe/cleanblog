ba5f1e6796e711713f54765d255ca51eee562f43
Ismael Toe <ismael.toe@gmail.com>
2015-01-06 17:39:14 +0000

    Bug fixed

--- content-page.php
+++ content-page.php
@@ -1,9 +1,16 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package Clean Blog
+ */
+?>
     <!-- Page Header -->
     <?php if ( has_post_thumbnail() ) : ?>
         <?php $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( '1900', '9999' ) ); ?>
         <header class="intro-header" style="background-image: url('<?php echo $image_url[0]; ?>')">
     <?php else : ?>
-        <header class="intro-header" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/home-bg.jpg')">
+        <header class="intro-header" style="background-image: url('<?php header_image(); ?>')">
     <?php endif; ?>
         <div class="container">
             <div class="row">
@@ -23,27 +30,26 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <?php 
                     the_content();
+
                     wp_link_pages( array(
-                        'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', TDOMAIN ) . '</span>',
-                        'after'       => '</div>',
-                        'link_before' => '<span>',
-                        'link_after'  => '</span>',
+                        'before' => '<div class="page-links">' . __( 'Pages:', 'cleanblog' ),
+                        'after'  => '</div>',
                     ) );
 
-                    edit_post_link( __( 'Edit', TDOMAIN ), '<span class="edit-link">', '</span>' );
+                    edit_post_link( __( 'Edit', 'cleanblog' ), '<span class="edit-link">', '</span>' );
                 ?>
             </div>
         </div>
 
-        <?php if ( comments_open() || '0' != get_comments_number() ) : ?>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div>
-                        <?php comments_template(); ?>
-                    </div>
+        <?php if ( comments_open() || get_comments_number() ) : ?>
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div>
+                    <?php comments_template(); ?>
                 </div>
             </div>
-            <?php endif; ?>
+        </div>
+        <?php endif; ?>
     </div>
 
     <hr>

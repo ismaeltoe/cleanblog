cab37c34660d6ccb68870753f6e519fa6fed1aa8
Ismael Toe <ismael.toe@gmail.com>
2015-01-06 18:04:12 +0000

    Improvements

--- header.php
+++ header.php
@@ -1,27 +1,20 @@
-<!DOCTYPE html>
+<?php
+/**
+ * The header for our theme.
+ *
+ *
+ *
+ * @package Clean Blog
+ */
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
 <head>
-
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
-
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-    <!-- Bootstrap Core CSS -->
-    <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -40,7 +33,7 @@
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only"><?php _e( 'Toggle navigation', TDOMAIN ); ?></span>
+                    <span class="sr-only"><?php _e( 'Toggle navigation', 'cleanblog' ); ?></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>

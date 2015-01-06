579c37865a1b1006d37100bf5924fe7bb76ef7ac
Ismael Toe <ismael.toe@gmail.com>
2015-01-06 18:18:13 +0000

    Improvements

--- page.php
+++ page.php
@@ -1,13 +1,21 @@
-<?php get_header(); ?>
-    
-    <?php if ( have_posts() ) { ?>
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Clean Blog
+ */
 
-        <?php while ( have_posts() ) : the_post(); ?>
+get_header(); ?>
 
-            <?php get_template_part( 'content', 'page' ); ?>
+    <?php while ( have_posts() ) : the_post(); ?>
 
-        <?php endwhile; ?>
+        <?php get_template_part( 'content', 'page' ); ?>
 
-    <?php } ?>
+    <?php endwhile; ?>
 
 <?php get_footer(); ?>

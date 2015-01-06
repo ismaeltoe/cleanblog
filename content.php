276c4f27375debb616a6edb9195cdeced043b69d
Ismael Toe <ismael.toe@gmail.com>
2015-01-06 17:28:41 +0000

    Updated

--- content.php
+++ content.php
@@ -1,14 +1,24 @@
+<?php
+/**
+ * @package Clean Blog
+ */
+?>
+
 <div id="post-<?php the_ID(); ?>" <?php post_class( 'post-preview' ); ?>>
 
     <a href="<?php the_permalink(); ?>" rel="bookmark">
         <h2 class="post-title"><?php the_title(); ?></h2>
         <h3 class="post-subtitle"><?php echo get_the_excerpt(); ?></h3>
     </a>
-
-    <p class="post-meta"><?php _e( 'Posted by', TDOMAIN ); ?> <?php the_author_posts_link(); ?> <?php _e( 'on', TDOMAIN ); ?> <?php the_time( get_option( 'date_format' ) ); ?></p>
+	
+	<?php if ( 'post' == get_post_type() ) : ?>
+    <p class="post-meta">
+    	<?php cleanblog_posted_on(); ?>
+   	</p>
+	<?php endif; ?>
 
     <div class="entry-meta">
-		<?php edit_post_link( __( 'Edit', TDOMAIN ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'cleanblog' ), '<span class="edit-link">', '</span>' ); ?>
     </div>
 
 </div>

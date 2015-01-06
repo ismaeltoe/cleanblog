38d455ceaf45c258af190689334851286b45468c
Ismael Toe <ismael.toe@gmail.com>
2015-01-06 18:44:27 +0000

    Improvements

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
  */
 ?>
     <!-- Footer -->
-    <footer>
+    <footer class="site-footer">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
